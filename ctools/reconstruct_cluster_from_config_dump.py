kaloianm/workscripts

commit c59869e7ae71ec6d3b0e0d78978c2f5be4a0135a

Make reconstruct_cluster_from_config_dump.py ignore missing UUIDs

ctools/reconstruct_cluster_from_config_dump.py
@@ -253,7 +253,8 @@ def main():
             collectionParts = collection['_id'].split('.', 1)
             dbName = collectionParts[0]
             collName = collectionParts[1]
-            collUUID = collection['uuid']
+            collUUID = collection['uuid'] if 'uuid' in collection else None
+
             shardKey = collection['key']
 
             db = shardConnection.get_database(dbName)
@@ -265,9 +266,12 @@ def main():
                     'o': {
                         'create': collName,
                     },
-                    'ui': collUUID,
                 }]
             }
+
+            if collUUID:
+                applyOpsCommand['ui'] = collUUID
+
             config.log_line("db.adminCommand(" + str(applyOpsCommand) + ");")
             db.command(applyOpsCommand, codec_options=CodecOptions(uuid_representation=4))
 

commit 8098ee691b0e7f0e907b59cffee8f5e35f9ab888

reconstruct_cluster_from_config_dump: Fix UUID creation
Fix an issue introduced in c59869e7ae71ec6d3b0e0d78978c2f5be4a0135a
 'Make reconstruct_cluster_from_config_dump.py ignore missing UUIDs'

Incorrect UUID field in c59869e7ae71e:
  {'applyOps': [{'op': '', 'ns': '', 'o': {'create': ''}}], 'ui': UUID()}

Correct UUID field:
  {'applyOps': [{'op': '', 'ns': '', 'o': {'create': ''}, 'ui': UUID()}]}

ctools/reconstruct_cluster_from_config_dump.py
@@ -270,7 +270,7 @@ def main():
             }
 
             if collUUID:
-                applyOpsCommand['ui'] = collUUID
+                applyOpsCommand['applyOps'][0]['ui'] = collUUID
 
             config.log_line("db.adminCommand(" + str(applyOpsCommand) + ");")
             db.command(applyOpsCommand, codec_options=CodecOptions(uuid_representation=4))