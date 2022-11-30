82a68e80a738bd5c8ca5609bc2a2bd4901207caa
Tommaso Tocci <tommaso.tocci@mongodb.com>
2022-11-30 20:31:00 +0100

    [auto-merger] Add scoping to hide internal script definitions
    https://www.mongodb.com/docs/mongodb-shell/write-scripts/scoping/

--- ctools/auto_merge_chunks.js
+++ ctools/auto_merge_chunks.js
@@ -7,65 +7,68 @@
  *
  */
 
-const NS = 'test.coll';
-const MAX_CHUNKS_PER_MERGE = 1000;
-const SLEEP_TIME_MS = 1000;
+'use strict';
+(() => {
+    const NS = 'test.coll';
+    const MAX_CHUNKS_PER_MERGE = 1000;
+    const SLEEP_TIME_MS = 1000;
 
-/* ============= DO NOT MODIFY NEXT LINES ===================== */
+    /* ============= DO NOT MODIFY NEXT LINES ===================== */
 
-function logLine(str) {
-    print('[AUTO-MERGER] ' + str);
-}
+    function logLine(str) {
+        print('[AUTO-MERGER] ' + str);
+    }
 
-if (db.version() < "6.0.3") {
+    if (db.version() < "6.0.3") {
         logLine("ERROR: Incompatible mongodb version (" + db.version() + "). The auto merger script is compatible only with versions >= 6.0.3";
         return;
-}
+    }
 
-if (db.adminCommand({balancerStatus: 1}).mode !== 'off') {
+    if (db.adminCommand({balancerStatus: 1}).mode !== 'off') {
         logLine("ERROR: The balancer is enabled. Disable the balancer before to run the auto merger script);
         return;
-}
-
-logLine('Starting for collection ' + NS + ' - Max chunks to squash per merge request: ' +
-        MAX_CHUNKS_PER_MERGE + ' - Sleep between each merge request: ' + SLEEP_TIME_MS + 'ms');
-
-const config = db.getSiblingDB('config');
-const collectionDoc = config.collections.findOne({_id: NS});
-const collUUID = collectionDoc.uuid;
-const chunks = config.chunks.find({uuid: collUUID}).sort({min: 1}).noCursorTimeout();
-
-var chunksToMerge = [];
-var numChunksScanned = 0;
-
-function issueMergeRequest() {
-    if (chunksToMerge.length > 1) {
-        const min = chunksToMerge[0].min;
-        const max = chunksToMerge[chunksToMerge.length - 1].max;
-        const shard = chunksToMerge[0].shard;
-        db.getSiblingDB("admin").runCommand({mergeChunks: NS, bounds: [min, max]});
-        logLine('MERGED ' + chunksToMerge.length + ' CHUNKS FROM ' + JSON.stringify(min) + ' TO ' +
-                JSON.stringify(max) + ' ON SHARD ' + shard);
-        logLine('CHUNKS PROCESSED SO FAR: ' + numChunksScanned);
-        sleep(SLEEP_TIME_MS);
-    }
-    chunksToMerge = [];
-}
-
-while (chunks.hasNext()) {
-    var chunk = chunks.next();
-
-    numChunksScanned++;
-
-    if (chunksToMerge.length == MAX_CHUNKS_PER_MERGE ||
-        (chunksToMerge.length > 0 &&
-         chunksToMerge[chunksToMerge.length - 1].shard != chunk.shard)) {
-        issueMergeRequest();
     }
 
-    chunksToMerge.push(chunk);
-}
+    logLine('Starting for collection ' + NS + ' - Max chunks to squash per merge request: ' +
+            MAX_CHUNKS_PER_MERGE + ' - Sleep between each merge request: ' + SLEEP_TIME_MS + 'ms');
 
-issueMergeRequest();
+    const config = db.getSiblingDB('config');
+    const collectionDoc = config.collections.findOne({_id: NS});
+    const collUUID = collectionDoc.uuid;
+    const chunks = config.chunks.find({uuid: collUUID}).sort({min: 1}).noCursorTimeout();
 
-logLine(' TOTAL CHUNKS PROCESSED: ' + numChunksScanned);
+    var chunksToMerge = [];
+    var numChunksScanned = 0;
+
+    function issueMergeRequest() {
+        if (chunksToMerge.length > 1) {
+            const min = chunksToMerge[0].min;
+            const max = chunksToMerge[chunksToMerge.length - 1].max;
+            const shard = chunksToMerge[0].shard;
+            db.getSiblingDB("admin").runCommand({mergeChunks: NS, bounds: [min, max]});
+            logLine('MERGED ' + chunksToMerge.length + ' CHUNKS FROM ' + JSON.stringify(min) +
+                    ' TO ' + JSON.stringify(max) + ' ON SHARD ' + shard);
+            logLine('CHUNKS PROCESSED SO FAR: ' + numChunksScanned);
+            sleep(SLEEP_TIME_MS);
+        }
+        chunksToMerge = [];
+    }
+
+    while (chunks.hasNext()) {
+        var chunk = chunks.next();
+
+        numChunksScanned++;
+
+        if (chunksToMerge.length == MAX_CHUNKS_PER_MERGE ||
+            (chunksToMerge.length > 0 &&
+             chunksToMerge[chunksToMerge.length - 1].shard != chunk.shard)) {
+            issueMergeRequest();
+        }
+
+        chunksToMerge.push(chunk);
+    }
+
+    issueMergeRequest();
+
+    logLine(' TOTAL CHUNKS PROCESSED: ' + numChunksScanned);
+})()
