d3e8846b90eb7d3d6145b1ab13e527d639c99bf5
Kaloian Manassiev <kaloian.manassiev@mongodb.com>
2026-04-23 15:43:40 +0000

    Add commands to the comments

--- ctools/locust_read_write_load.py
+++ ctools/locust_read_write_load.py
@@ -10,7 +10,13 @@
 #   mgodatagen -f locust_read_write_load_mgodatagen_100GB.json --uri mongodb://localhost
 #
 #   # Then, run the workload (uses Locust's built-in --processes for multi-worker support):
-#   locust -f locust_read_write_load.py --processes 4 --users 1000 --spawn-rate 25 --autostart --web-port 8090 --csv=locust_results --mgodatagen-config locust_read_write_load_mgodatagen_100GB.json --host mongodb://localhost
+#   locust -f locust_read_write_load.py --processes 4 --users 1000 --spawn-rate 25 --autostart --web-port 8090 --csv=locust_results --html=locust_results.html --print-stats --mgodatagen-config locust_read_write_load_mgodatagen_100GB.json --host mongodb://localhost
+#
+#   # Curl commands
+#   curl -sX POST http://localhost:8090/custom_actions/deleteMany_10_pct | jq -r .command
+#   curl -sX POST http://localhost:8090/custom_actions/fastBulkDelete_10_pct | jq -r .command
+#
+#   curl -L -o report.html "http://localhost:8090/stats/report?download=1&theme=dark"
 #
 
 import json
