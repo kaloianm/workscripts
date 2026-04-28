bcf915b265a68b5ac39409c4d8c718b9742fafae
Kaloian Manassiev <kaloian.manassiev@mongodb.com>
2026-04-28 07:43:02 +0000

    Change the number of users to 500

--- ctools/locust_workload.py
+++ ctools/locust_workload.py
@@ -11,8 +11,8 @@
 #   mgodatagen -f locust_workload_mgodatagen_100GB_autoinc.json --uri mongodb://localhost
 #
 #   # Then, run the workload (uses Locust's built-in --processes for multi-worker support):
-#   locust -f locust_workload.py --processes 4 --users 1000 --spawn-rate 25 --autostart --web-port 8090 --csv=locust_results --html=locust_results_report.html --print-stats --mgodatagen-config locust_workload_mgodatagen_100GB.json --host mongodb://localhost
-#   locust -f locust_workload.py --processes 4 --users 1000 --spawn-rate 25 --autostart --web-port 8090 --csv=locust_results --html=locust_results_report.html --print-stats --mgodatagen-config locust_workload_mgodatagen_100GB_autoinc.json --host mongodb://localhost
+#   locust -f locust_workload.py --processes 4 --users 500 --spawn-rate 25 --autostart --web-port 8090 --csv=locust_results --html=locust_results_report.html --print-stats --mgodatagen-config locust_workload_mgodatagen_100GB.json --host mongodb://localhost
+#   locust -f locust_workload.py --processes 4 --users 500 --spawn-rate 25 --autostart --web-port 8090 --csv=locust_results --html=locust_results_report.html --print-stats --mgodatagen-config locust_workload_mgodatagen_100GB_autoinc.json --host mongodb://localhost
 #
 #   # Curl commands
 #   curl -sX POST http://localhost:8090/custom_actions/deleteMany_10_pct | jq -r .command
